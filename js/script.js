f08933a52f6ca7ee1d82bedee0f9fbecc874c1f3
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-12 14:29:52 -0500

    JavaScript Final Assignment
    Added JS to portfolio site. Slideshow, form pop-up, and button functionality. changed some css and html, added images and captions, as well as a quote

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// function that shows first slide on page load(in html) and allows for the changing of slides
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides"); 
+    var dots = document.getElementsByClassName("dot"); 
+    if (n > slides.length) {slideIndex = 1}; 
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block"; 
+    dots[slideIndex - 1].className += " active"; 
+}
